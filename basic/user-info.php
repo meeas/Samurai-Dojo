63ac3a25b9e23f1159c03c9aa583e50c98a35b3e
Jason Gillam <jason.gillam@gmail.com>
2016-11-04 17:30:58 -0400

    Fixed form names

--- basic/user-info.php
+++ basic/user-info.php
@@ -9,6 +9,7 @@ if ($password <> "") {
 	$result = mysql_query($query) or die(mysql_error($conn) . '<p><b>SQL Statement:</b>' . $query);
 	header("Location: ".$_SERVER['SCRIPT_NAME']."?".$_SERVER['QUERY_STRING']);
 }
+
 ?>
 
 <?php
@@ -21,19 +22,19 @@ if (mysql_num_rows($result) > 0) {
     <div class="form-group">
         <label for="username" class="col-sm-2 control-label">Username:</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputUsername" placeholder="username" value="<?php echo "{$row['username']}" ?>" disabled>
+            <input type="text" class="form-control" name="username" id="username" placeholder="username" value="<?php echo "{$row['username']}" ?>" disabled>
         </div>
     </div>
     <div class="form-group">
         <label for="password" class="col-sm-2 control-label">Password:</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" placeholder="********">
+            <input type="password" class="form-control" name="password" id="password" placeholder="********">
         </div>
     </div>
     <div class="form-group">
         <label for="signature" class="col-sm-2 control-label">Signature:</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputSignature" value="<?php echo "{$row['mysignature']}" ?>">
+            <input type="text" class="form-control" name="signature" id="signature" value="<?php echo "{$row['mysignature']}" ?>">
         </div>
     </div>
 
@@ -42,7 +43,7 @@ if (mysql_num_rows($result) > 0) {
       <button type="submit" name="Submit_button" class="btn btn-default">Update</button>
     </div>
   </div>
-
+</form>
       <?php
 	}
 } else {
@@ -58,13 +59,13 @@ if ($_COOKIE["showhints"]==1) {
 	<b>For SQL Injection:</b>The old "\' or 1=1 -- " is a classic, but there are others. This just shows
 	you too much info. Also try an injection like this on the <a href="?page=login.php">Login</a> page.
 	<br><br>
-	<b>For Session and Authentication:</b>As for playing with sessions, try a 
-	<a href="https://addons.mozilla.org/en-US/firefox/addon/4510">cookie editor</a> 
+	<b>For Session and Authentication:</b>As for playing with sessions, try a
+	<a href="https://addons.mozilla.org/en-US/firefox/addon/4510">cookie editor</a>
 	to change your UID.
 	<br><br>
 	<b>For XSS:</b> You may also see an XSS attack you left when you used the
 	registration page.
-	</span>'; 
+	</span>';
 }
 // End hints section
 ?>
